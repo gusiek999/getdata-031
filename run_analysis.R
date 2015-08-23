94f80cae9da97eb3ad9364116a2cd058fd1afbe4
gusiek999 <gusiek999@users.noreply.github.com>
2015-08-23 18:51:44 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -12,6 +12,7 @@ y_test <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
 
 ### TASK 1
+### Merges the training and the test sets to create one data set.
 subject <- rbind(subject_train, subject_test)
 activities <- rbind(y_train, y_test)
 features <- rbind(X_train, X_test)
@@ -21,6 +22,7 @@ colnames(subject) <- "Subject"
 merge_data <- cbind(features,activities,subject)
 
 ### TASK 2
+### Extracts only the measurements on the mean and standard deviation for each measurement. 
 mean_std <- grep(".*Mean.*|.*Std.*", names(merge_data), ignore.case=TRUE)
 mean_std_new <- c(mean_std, 562, 563)
 #dim(merge_data)
@@ -29,6 +31,7 @@ extracts <- merge_data[,mean_std_new]
 #test<-merge_data[,mean_std]
 
 ### TASK 3
+### Uses descriptive activity names to name the activities in the data set
 extracts$Activity <- as.character(extracts$Activity)
 for (i in 1:6){
   extracts$Activity[extracts$Activity == i] <- as.character(activity[i,2])
@@ -36,7 +39,7 @@ for (i in 1:6){
 extracts$Activity <- as.factor(extracts$Activity)
 
 ### TASK 4
-#names(extracts)
+### Appropriately labels the data set with descriptive variable names. 
 names(extracts)<-gsub("Acc", "Accelerometer", names(extracts))
 names(extracts)<-gsub("Gyro", "Gyroscope", names(extracts))
 names(extracts)<-gsub("-mean()", "Mean", names(extracts), ignore.case = TRUE)
@@ -51,6 +54,7 @@ names(extracts)<-gsub("-freq()", "Frequency", names(extracts), ignore.case = TRU
 names(extracts)<-gsub("angle", "Angle", names(extracts))
 
 ### TASK 5
+### From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 extracts$Subject <- as.factor(extracts$Subject)
 extracts <- data.table(extracts)
 tidy_data <- aggregate(. ~Subject + Activity, extracts, mean)
